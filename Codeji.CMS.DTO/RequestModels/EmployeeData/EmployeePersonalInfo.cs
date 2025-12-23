df32f25e160e7cb9b9d448e5788073e409e0896f
Robin Singh <robin.singh@codeji.in>
2025-12-23 18:46:14 +0530

    fix: change Gender property to be optional in EmployeePersonalInfo model

--- Codeji.CMS.DTO/RequestModels/EmployeeData/EmployeePersonalInfo.cs
+++ Codeji.CMS.DTO/RequestModels/EmployeeData/EmployeePersonalInfo.cs
@@ -7,7 +7,7 @@ namespace Codeji.CMS.DTO.RequestModels.EmployeeData
     {
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
-        public required string Gender { get; set; }
+        public string? Gender { get; set; }
         public required string RoleId { get; set; }
         public string? DateOfBirth { get; set; }
         public string? DateOfJoining { get; set; }
